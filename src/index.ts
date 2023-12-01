233f01f2cf18937c2a4111634ac69dee1103690c
n-e-o-n-7 <44neon77@gmail.com>
2023-12-01 09:42:03 +0800

    :sparkles: base

--- src/index.ts
+++ src/index.ts
@@ -9,24 +9,43 @@
  */
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	r2: R2Bucket;
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		const key = url.pathname.slice(1);
+
+		switch (request.method) {
+			case 'PUT':
+				await env.r2.put(key, request.body);
+				return new Response(`Put ${key} successfully!`);
+			case 'GET':
+				const object = await env.r2.get(key);
+
+				if (object === null) {
+					return new Response('Object Not Found', { status: 404 });
+				}
+
+				const headers = new Headers();
+				object.writeHttpMetadata(headers);
+				headers.set('etag', object.httpEtag);
+
+				return new Response(object.body, {
+					headers,
+				});
+			case 'DELETE':
+				await env.r2.delete(key);
+				return new Response('Deleted!');
+
+			default:
+				return new Response('Method Not Allowed', {
+					status: 405,
+					headers: {
+						Allow: 'PUT, GET, DELETE',
+					},
+				});
+		}
 	},
 };
